0f0106575df56080a5b490d3f8b541a2025ff212
bsef19a522 <bsef19a522@pucit.edu.pk>
2022-05-21 12:25:53 +0500

    SuccessFull

---
+++ app/src/main/java/com/example/assignment/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.assignment;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button btn = (Button) findViewById(R.id.button);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Uri uri = Uri.parse("https://github.com/bsef19a522?tab=repositories");
+                Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+                startActivity(intent);
+            }
+        });
+    }
+}
